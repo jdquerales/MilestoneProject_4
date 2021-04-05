24996ba411ddffbce94dd36697b09042f77822cd
jdquerales <jquerales72@gmail.com>
2021-04-05 15:09:34 +0000

    Add unit test for origin and location models in products app

--- products/models.py
+++ products/models.py
@@ -34,17 +34,6 @@ class Location(models.Model):
         return self.friendly_name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
-
-
 class Product(models.Model):
     description = models.TextField()
     friendly_name = models.CharField(max_length=254)
--- products/tests_models.py
+++ products/tests_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, Origin, Location
 
 # Create your tests here.
 class TestModels(TestCase):
@@ -8,29 +8,80 @@ class TestModels(TestCase):
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
         Category.objects.create(name='Test Category', friendly_name='Test friendly name')
+        Origin.objects.create(name='Test Origin', friendly_name='Test Origin friendly name')
+        Location.objects.create(name='Test Location', friendly_name='Test Location friendly name')
     
-    def test_name_label(self):
+    def test_category_name_label(self):
         name = Category.objects.get(pk=1)
         field_label = name._meta.get_field('name').verbose_name
         self.assertEqual(field_label, 'name')
 
-    def test_friendly_name_label(self):
+    def test_category_friendly_name_label(self):
         name = Category.objects.get(pk=1)
         field_label = name._meta.get_field('friendly_name').verbose_name
         self.assertEqual(field_label, 'friendly name')
 
-    def test_name_max_length(self):
+    def test_category_name_max_length(self):
         name = Category.objects.get(id=1)
         max_length = name._meta.get_field('name').max_length
         self.assertEqual(max_length, 254)
 
-    def test_friendly_name_max_length(self):
+    def test_category_friendly_name_max_length(self):
         friendly_name = Category.objects.get(pk=1)
         max_length = friendly_name._meta.get_field('friendly_name').max_length
         self.assertEqual(max_length, 254)
 
-    def test_string_method(self):
+    def test_category_string_method(self):
         item = Category.objects.get(pk=1)
         expected_object_name = f'{item.name}'
         self.assertEqual(expected_object_name, str(item))
 
+    def test_origin_name_label(self):
+        name = Origin.objects.get(pk=1)
+        field_label = name._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_origin_friendly_name_label(self):
+        name = Origin.objects.get(pk=1)
+        field_label = name._meta.get_field('friendly_name').verbose_name
+        self.assertEqual(field_label, 'friendly name')
+
+    def test_origin_name_max_length(self):
+        name = Origin.objects.get(id=1)
+        max_length = name._meta.get_field('name').max_length
+        self.assertEqual(max_length, 254)
+
+    def test_origin_friendly_name_max_length(self):
+        friendly_name = Origin.objects.get(pk=1)
+        max_length = friendly_name._meta.get_field('friendly_name').max_length
+        self.assertEqual(max_length, 254)
+
+    def test_origin_string_method(self):
+        item = Origin.objects.get(pk=1)
+        expected_object_name = f'{item.name}'
+        self.assertEqual(expected_object_name, str(item))
+
+    def test_location_name_label(self):
+        name = Location.objects.get(pk=1)
+        field_label = name._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_location_friendly_name_label(self):
+        name = Location.objects.get(pk=1)
+        field_label = name._meta.get_field('friendly_name').verbose_name
+        self.assertEqual(field_label, 'friendly name')
+
+    def test_location_name_max_length(self):
+        name = Location.objects.get(id=1)
+        max_length = name._meta.get_field('name').max_length
+        self.assertEqual(max_length, 254)
+
+    def test_location_friendly_name_max_length(self):
+        friendly_name = Location.objects.get(pk=1)
+        max_length = friendly_name._meta.get_field('friendly_name').max_length
+        self.assertEqual(max_length, 254)
+
+    def test_location_string_method(self):
+        item = Location.objects.get(pk=1)
+        expected_object_name = f'{item.name}'
+        self.assertEqual(expected_object_name, str(item))
