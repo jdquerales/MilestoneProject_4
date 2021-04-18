dfda8751e016a30771678035b63cbd02b6b2cf59
jdquerales <jquerales72@gmail.com>
2021-04-18 12:27:33 +0000

    Add sort by price, name, category functionality in products page

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -12,8 +13,24 @@ def all_products(request):
     products = Product.objects.all()  # This method will return all products from the
     query = None
     categories = None
+    sort = None
+    ordering = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'friendly_name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('friendly_name'))
+
+            if 'ordering' in request.GET:
+                ordering = request.GET['ordering']
+                if ordering == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -29,10 +46,13 @@ def all_products(request):
                 description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{ordering}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -42,7 +62,7 @@ def product_detail(request, product_id):
     """ A view to retrieve a particular product by its id"""
 
     product = get_object_or_404(Product, pk=product_id)
-    # to get a particular product by id 
+    # to get a particular product by id
 
     context = {
         'product': product,
