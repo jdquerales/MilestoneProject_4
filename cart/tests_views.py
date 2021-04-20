626afddebe95950f4b840684b9d8668bcc17b6a3
jdquerales <jquerales72@gmail.com>
2021-04-20 15:51:08 +0000

    Add unit test for cart view

---
+++ cart/tests_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_get_home_page(self):
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart/cart.html')
