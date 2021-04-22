2a2c704b999f9d3626b13007a8f7e95d86092d7a
jdquerales <jquerales72@gmail.com>
2021-04-22 13:26:40 +0000

    Add form tag to quantity button

--- cart/views.py
+++ cart/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def view_cart(request):
     """ A view to render cart.html template for the shopping cart page """
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add the quantity of selected product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
