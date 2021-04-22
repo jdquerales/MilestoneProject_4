69f074e163bc63ce5e0d16037070ffb6a54dbc9f
jdquerales <jquerales72@gmail.com>
2021-04-22 18:52:31 +0000

    Add unit test to assert 302 response from add_to_cart view

--- cart/tests_views.py
+++ cart/tests_views.py
@@ -9,14 +9,25 @@ class TestViews(TestCase):
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
         Product.objects.create(description='first product',
-                               friendly_name='product', price=100, brand='some')
+                               friendly_name='product', 
+                               price=100, brand='some')
 
     def test_get_shopping_cart_page(self):
         response = self.client.get('/cart/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'cart/cart.html')
 
+    def test_get_add_to_cart_page_302(self):
+        product = Product.objects.get(pk=1)
+        response = self.client.post(f'/cart/add/{product.id}/',
+                                    {'quantity':'1', 
+                                    'redirect_url':f'/products/{product.id}'})
+        self.assertEquals(response.status_code, 302)
+
     def test_get_add_to_cart_page(self):
         product = Product.objects.get(pk=1)
-        response = self.client.post(f'/cart/add/{product.id}/', {'quantity':'1', 'redirect_url':f'/products/{product.id}'})
+        response = self.client.post(f'/cart/add/{product.id}/',
+                                    {'quantity':'1', 
+                                    'redirect_url':f'/products/{product.id}'})
         self.assertRedirects(response, f'/products/{product.id}')
+
