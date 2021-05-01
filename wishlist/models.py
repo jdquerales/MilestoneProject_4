ab3fb1b59491306038b3e1642ac5ca690af0344e
jdquerales <jquerales72@gmail.com>
2021-05-01 17:47:05 +0000

    Make migrations for wishlist app & models

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class WishManager(models.Manager):
+    def validateWish(self, post_data):
+        is_valid = True
+        errors = []
+        if len(post_data.get('item')) < 3:
+            is_valid = False
+            errors.append('Message must be more than 3 characters')
+            return (is_valid, errors)
+
+class Wish(models.Model):
+ #   user = models.ForeignKey(User, related_name="wishes")
+    item = models.CharField(max_length=255)
+#    wishers = models.ManyToManyField(User, related_name="items")
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    objects = WishManager()
