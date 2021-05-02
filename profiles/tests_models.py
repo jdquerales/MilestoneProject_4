759bb06449ef9ede278f1c6c0a1070cc34c68a41
jdquerales <jquerales72@gmail.com>
2021-05-02 19:08:49 +0000

    Add unit test to check that User field in profile model is a instance of User

--- profiles/tests_models.py
+++ profiles/tests_models.py
@@ -8,7 +8,11 @@ class TestViews(TestCase):
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
         User.objects.create(username='user_test')
-
+        
     def test_username(self):
         user = User.objects.get(pk=1)
         self.assertEqual(user.username, 'user_test')
+
+    def test_name_label(self):
+        profile = UserProfile.objects.get(pk=1)
+        self.assertIsInstance(profile.user, User)
