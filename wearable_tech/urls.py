8da7dc37c1794cf6ad0a1a1ed11a1c631943f4e3
jdquerales <jquerales72@gmail.com>
2021-05-02 18:09:41 +0000

    Create profiles app, views and urls

--- wearable_tech/urls.py
+++ wearable_tech/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('coupons/', include('coupons.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
