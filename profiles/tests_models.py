f0617e6d4f8184d8311fe58241a5987c65988524
jdquerales <jquerales72@gmail.com>
2021-05-02 19:27:10 +0000

    Add unit test for UserProfile fields metadata and string method

--- profiles/tests_models.py
+++ profiles/tests_models.py
@@ -13,6 +13,32 @@ class TestViews(TestCase):
         user = User.objects.get(pk=1)
         self.assertEqual(user.username, 'user_test')
 
-    def test_name_label(self):
+    def test_user_field_instance(self):
         profile = UserProfile.objects.get(pk=1)
         self.assertIsInstance(profile.user, User)
+
+    def test_default_phone_number_label(self):
+        phone = UserProfile.objects.get(pk=1)
+        field_label = phone._meta.get_field('default_phone_number').verbose_name
+        self.assertEqual(field_label, 'default phone number')
+
+    def test_default_street_address1_label(self):
+        address1 = UserProfile.objects.get(pk=1)
+        field_label = address1._meta.get_field('default_street_address1').verbose_name
+        self.assertEqual(field_label, 'default street address1')
+
+    def test_default_street_address2_label(self):
+        address2 = UserProfile.objects.get(pk=1)
+        field_label = address2._meta.get_field('default_street_address2').verbose_name
+        self.assertEqual(field_label, 'default street address2')
+
+    def test_default_town_or_city_label(self):
+        city = UserProfile.objects.get(pk=1)
+        field_label = city._meta.get_field('default_town_or_city').verbose_name
+        self.assertEqual(field_label, 'default town or city')
+
+    def test_profile_string_method(self):
+        profile = UserProfile.objects.get(pk=1)
+        user = User.objects.get(pk=1)
+        expected_object_name = f'{user.username}'
+        self.assertEqual(expected_object_name, str(user))
