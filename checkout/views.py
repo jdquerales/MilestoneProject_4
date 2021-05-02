ed070cfa28d910aadc02437b6d530c96fc6c2370
jdquerales <jquerales72@gmail.com>
2021-05-02 09:59:23 +0000

    Implemented stripe payment elements in checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'There is nothing in your cart at the moment')
@@ -14,6 +18,8 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
