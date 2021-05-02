c54a8ea63a8d545c62c60f219edcb4cb400b16e0
jdquerales <jquerales72@gmail.com>
2021-05-02 18:31:50 +0000

    Add unit test for User creation

---
+++ profiles/tests_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class TestViews(TestCase):
+# Set up non-modified objects used by all test methods
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(username='user_test')
+
+    def test_username(self):
+        user = User.objects.get(pk=1)
+        self.assertEqual(user.username, 'user_test')
