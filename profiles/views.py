b454af7bd0557d8a6194658f59a6832af1a8f25e
jdquerales <jquerales72@gmail.com>
2021-05-03 11:12:52 +0000

    Add css styles for verification sent page

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
+from .models import UserProfile
 
 def profile(request):
     """Display the user's profile"""
     template = 'profiles/profile.html'
     
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     wishlist = Product.objects.filter(user_wishlist=request.user)
     # Here I retrieve all products from user wishlist
     print(wishlist)
 
-    context = {'wishlist': wishlist,}
+    context = {
+        'profile': profile,
+        'wishlist': wishlist,}
     
     return render(request, template, context)
     
