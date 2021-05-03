ce6b5253f5288889adec3e8541a4768aea0f9dcc
jdquerales <jquerales72@gmail.com>
2021-05-03 15:19:49 +0000

    Fix AWS bucket name in settings.py

--- wearable_tech/settings.py
+++ wearable_tech/settings.py
@@ -189,7 +189,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket  Config
-    AWS_STORAGE_BUCKET_NAME = 'jose-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'ms4-smart-tech'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
