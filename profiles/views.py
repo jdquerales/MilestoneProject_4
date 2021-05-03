6c847fd2ec246f5240c0aebb63735bfc9e58d473
jdquerales <jquerales72@gmail.com>
2021-05-03 12:14:13 +0000

    Add update profile information functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from products.models import Product
 from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
     """Display the user's profile"""
     template = 'profiles/profile.html'
-    
+
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+    
+    
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     wishlist = Product.objects.filter(user_wishlist=request.user)
     # Here I retrieve all products from user wishlist
     print(wishlist)
+    print(orders)
 
     context = {
         'profile': profile,
-        'wishlist': wishlist,}
+        'form': form,
+        'wishlist': wishlist,
+        'orders': orders}
     
     return render(request, template, context)
     
