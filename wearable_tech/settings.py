0f0eb29d91d189fb44a0c550f7b0717a393d943b
jdquerales <jquerales72@gmail.com>
2021-05-05 08:21:05 +0000

    Unabl Xframe django setting temporary

--- wearable_tech/settings.py
+++ wearable_tech/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
